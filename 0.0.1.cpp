af906f5253fa95216fcd741b6194e45d3f969cbe
ArtyomGorbackij <31548827+ArtyomGorbackij@users.noreply.github.com>
2017-11-10 13:19:43 +0300

    Update 0.0.1.cpp

--- 0.0.1.cpp
+++ 0.0.1.cpp
@@ -1,35 +1,43 @@
-#include<sstream>
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <sstream>
 using namespace std;
-int n;
-int main()
+bool vvod(int & n, int * a)
 {
-    int err;
     string str;
-    bool fail;
-    cin >> n;
-    cin.ignore(1, '\n');
     getline(cin, str);
     istringstream stream(str);
-    int *a = new int[n];
-    for (int i = 0; i < n; i++) {
-        if (!(stream >> a[i]) || n<0) {
-            fail=true;
+    int element = -1;
+    bool fail = true;
+    for (int i = 0; i < n; ++i) {
+        if (!(stream >> a[i])) {
+            fail = false;
+            break;
         }
     }
-    if(!fail){
-        for (int i = 0; i < n / 2; i++) {
-            swap(a[i], a[n-1- i]);
+    if (stream >> element) fail = false;
+    return fail;
+}
+int main() {
+    int n;
+    string str;
+    getline(cin, str);
+    istringstream stream(str);
+    if (stream >> n) {
+        int k = abs(n);
+        int * a = new int[k];
+        if (vvod(n, a) && n > 0) {
+            for (int i = 0,j=n-1; i < n / 2 ; j--,i++) {
+                swap(a[i], a[j]);
+            }
+            for (int i = 0; i < n; i++) {
+                cout << a[i] << " ";
+            }
         }
+        else {
+            cout << "An error has occured while reading input data.";
+        }
+    delete[] a;
     }
-    else{
-        cout<<"An error has occured while reading input data.";
-        cin.get();
-        return 0;
-    }
-    for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
     cin.get();
     return 0;
 }
