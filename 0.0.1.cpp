a62636f7813ec152db408a75939341a42f3dfab5
ArtyomGorbackij <31548827+ArtyomGorbackij@users.noreply.github.com>
2017-11-10 09:33:29 +0300

    Create 0.0.1.cpp

---
+++ 0.0.1.cpp
@@ -0,0 +1,34 @@
+#include<sstream>
+#include<iostream>
+#include<cstring>
+    using namespace std;
+    int n;
+    int main()
+    {
+        string str;
+        bool fail;
+        cin >> n;
+        cin.ignore(1, '\n');
+        getline(cin, str);
+        istringstream stream(str);
+        int *a = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (!(stream >> a[i]) || n<0) {
+                fail=true;
+            }
+        }
+            if(!fail){
+                for (int i = 0; i < n / 2; i++) {
+                    swap(a[i], a[n-1- i]);
+                }
+            }
+            else{
+                cout<<"An error has occured while reading input data.";
+                cin.get();
+                return 0;
+            }
+        for (int i = 0; i < n; i++)
+            cout << a[i] << " ";
+        cin.get();
+        return 0;
+    }
